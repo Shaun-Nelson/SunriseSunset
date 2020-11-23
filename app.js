43816a31ff1e0acb1fa12746fcaba1a187adf057
Shaun <shaunptnelson@gmail.com>
2020-11-22 19:42:03 -0500

    Changed API

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-const apiURL = 'https://api.sunrise-sunset.org/json?';
+// const apiURL = 'https://api.sunrise-sunset.org/json?';
+// const apiURL = 'https://api.openweathermap.org/data/2.5/weather?';
+// const apiKey = '912f3dd2d8772ce1a1263d34855d3556';
+const apiURL = 'https://api.met.no/weatherapi/sunrise/2.0/.json?';
 
 function getAPIData(url) {
     fetch(url)
@@ -10,17 +13,30 @@ function getAPIData(url) {
 };
 
 function displayData(jsonResponse) {
-    document.getElementById('sunrise').innerHTML += formatTime(jsonResponse.results.sunrise);
-    document.getElementById('sunset').innerHTML += formatTime(jsonResponse.results.sunset);
+    let sunrise = jsonResponse.location.time[0].sunrise.time;
+    let sunset = jsonResponse.location.time[0].sunset.time;
+    document.getElementById('sunrise').innerHTML += formatSunriseTime(sunrise);
+    document.getElementById('sunset').innerHTML += formatSunsetTime(sunset);
 };
 
-function formatTime(time) {
-    let timeArr = time.split(':');
-    let numTime = parseInt(timeArr[0]) - 5;
-    timeArr[0] = numTime.toString();
-    
-    return timeArr.join(':');
+function formatSunriseTime(time) {
+    let date = new Date(time);
+    return (date.toTimeString().split(' ')[0] + ' AM').slice(1);
 };
+
+function formatSunsetTime(time) {
+    let date = new Date(time);
+    let output = date.toTimeString().split(' ')[0].split(':');
+    let hour = (output[0] - 12).toString();
+    output[0] = hour;
+
+    return output.join(':') + ' PM';
+};
+
+function getDate() {
+    let date = new Date
+    return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+}
 
 function getCoordinates() {
     return new Promise((res, rej) => {
@@ -35,10 +51,12 @@ function getCoordinates() {
 
 async function main() {
     try {
+    getDate();
     let coords = await getCoordinates()
     let lat = coords.coords.latitude;
     let lon = coords.coords.longitude;
-    let url = `${apiURL}lat=${lat}&lng=${lon}`;
+    let date = getDate();
+    let url = `${apiURL}lat=${lat}&lon=${lon}&date=${date}&offset=-05:00`;
     getAPIData(url);
     } catch (err) {
         console.log(err);
